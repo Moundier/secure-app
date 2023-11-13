f7dba55f314db043e6d7054bacc423b071b8262d
Moundier <garamoundier.developer@protonmail.com>
2023-11-13 02:20:43 -0300

    chore: add tab 0 to routing!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,20 @@ import { Tab2Component } from "./tab-2/tab-2.component";
 import { Tab3Component } from "./tab-3/tab-3.component";
 import { TabsComponent } from "./tabs/tabs.component";
 import { PageLoginComponent } from "./page-login/page-login.component";
+import { Tab0Component } from "./tab-0/tab-0.component";
 
 const routes: Routes = [
     { path: 'login', component: PageLoginComponent }, 
     {
         path: 'tabs',
-        redirectTo: 'tabs/tab-1', // Redirect to tab-1 by default
+        redirectTo: 'tabs/tab-0', // Redirect to tab-1 by default
         pathMatch: 'full',
     },
     {
         path: 'tabs',
         component: TabsComponent,
         children: [
+            { path: 'tab-0', component: Tab0Component, data: { animation: 'tab-0' } },
             { path: 'tab-1', component: Tab1Component, data: { animation: 'tab-1' } },
             { path: 'tab-2', component: Tab2Component, data: { animation: 'tab-2' } },
             { path: 'tab-3', component: Tab3Component, data: { animation: 'tab-3' } },
