0cd04e7b759764c37d017393a22305256f910a28
Moundier <garamoundier.developer@protonmail.com>
2023-11-13 02:19:21 -0300

    feat: tab 0!

---
+++ src/app/tab-0/tab-0.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tab-0',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tab-0.component.html',
+  styleUrl: './tab-0.component.css'
+})
+export class Tab0Component {
+
+}
